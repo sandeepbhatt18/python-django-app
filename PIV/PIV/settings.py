ad8b298677cfa48004bfd94e65e124c54a88649c
sandeep <sandeep.bhatt@gmail.com>
2019-02-15 19:46:48 +0530

    comment crispy form

--- PIV/PIV/settings.py
+++ PIV/PIV/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'survey_app',
-    'crispy_forms',
+    # 'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
